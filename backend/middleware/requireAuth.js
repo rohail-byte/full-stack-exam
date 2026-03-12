693886289ed901a9e1654674f6f57b36c5623059
rohail2012 <72384686+rohail2012@users.noreply.github.com>
2026-03-12 18:47:34 +0500

    Q7: Protect backend routes

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -1,3 +1,4 @@
+const requireAuth = require("../middleware/requireAuth");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
--- backend/routes/workoutRouter.js
+++ backend/routes/workoutRouter.js
@@ -8,19 +8,21 @@ const {
   deleteWorkout,
 } = require("../controllers/workoutControllers");
 
+const requireAuth = require("../middleware/requireAuth");
+
 // GET /api/workouts
 router.get("/", getAllWorkouts);
 
 // POST /api/workouts
-router.post("/", createWorkout);
+router.post("/", requireAuth, createWorkout);
 
 // GET /api/workouts/:workoutId
-router.get("/:workoutId", getWorkoutById);
+router.get("/:workoutId", requireAuth, getWorkoutById);
 
 // PUT /api/workouts/:workoutId
-router.put("/:workoutId", updateWorkout);
+router.put("/:workoutId", requireAuth, updateWorkout);
 
 // DELETE /api/workouts/:workoutId
-router.delete("/:workoutId", deleteWorkout);
+router.delete("/:workoutId", requireAuth, deleteWorkout);
 
 module.exports = router;
